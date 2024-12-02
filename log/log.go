c5c066133d192004d002bcc8e2a6de412f921e10
zngue <zngfue@163.com>
2024-12-02 16:47:42 +0800

    update

--- log/log.go
+++ log/log.go
@@ -1,1 +1,72 @@
 package log
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/utils"
+	"time"
+)
+
+type Log struct {
+	LogLevel logger.LogLevel
+}
+
+func (l *Log) LogMode(level logger.LogLevel) logger.Interface {
+	newLogger := *l
+	newLogger.LogLevel = level
+	return &newLogger
+}
+
+func (l *Log) Info(ctx context.Context, s string, i ...any) {
+	if l.LogLevel >= logger.Info {
+		logger.Default.Info(ctx, s, i...)
+	}
+}
+
+func (l *Log) Warn(ctx context.Context, s string, i ...any) {
+	if l.LogLevel >= logger.Warn {
+		logger.Default.Warn(ctx, s, i...)
+	}
+}
+
+func (l *Log) Error(ctx context.Context, s string, i ...any) {
+	if l.LogLevel >= logger.Error {
+		logger.Default.Error(ctx, s, i...)
+	}
+}
+
+func (l *Log) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
+	elapsed := time.Since(begin)
+	switch {
+	case err != nil && l.LogLevel >= logger.Error && (!errors.Is(err, gorm.ErrRecordNotFound)):
+		sql, rows := fc()
+		if rows == -1 {
+			// ErrRecordNotFound
+		} else {
+			l.Printf(l.traceErrStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, rows, sql)
+		}
+	case elapsed > l.SlowThreshold && l.SlowThreshold != 0 && l.LogLevel >= Warn:
+		sql, rows := fc()
+		slowLog := fmt.Sprintf("SLOW SQL >= %v", l.SlowThreshold)
+		if rows == -1 {
+			l.Printf(l.traceWarnStr, utils.FileWithLineNum(), slowLog, float64(elapsed.Nanoseconds())/1e6, "-", sql)
+		} else {
+			l.Printf(l.traceWarnStr, utils.FileWithLineNum(), slowLog, float64(elapsed.Nanoseconds())/1e6, rows, sql)
+		}
+	case l.LogLevel == Info:
+		sql, rows := fc()
+		if rows == -1 {
+			l.Printf(l.traceStr, utils.FileWithLineNum(), float64(elapsed.Nanoseconds())/1e6, "-", sql)
+		} else {
+			l.Printf(l.traceStr, utils.FileWithLineNum(), float64(elapsed.Nanoseconds())/1e6, rows, sql)
+		}
+	}
+}
+
+func NewLog() logger.Interface {
+
+	return new(Log)
+}
