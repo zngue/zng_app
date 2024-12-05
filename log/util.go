0708f946bb5bab11b6b213787e9f05d2a7852f2f
zngue <zngfue@163.com>
2024-12-05 10:00:30 +0800

    update

--- log/util.go
+++ log/util.go
@@ -1,26 +1,64 @@
 package log
 
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"time"
+)
+
+func logF(s string, i ...any) (data []zap.Field) {
+	begin := time.Now()
+	data = append(data, zap.String("elapsed", begin.Format("2006-01-02 15:04:05")))
+	data = append(data, zap.Any("log_data", fmt.Sprintf(s, i...)))
+	return
+}
+func log(i ...any) (data []zap.Field) {
+	begin := time.Now()
+	if len(i) == 0 {
+		return
+	}
+	data = append(data, zap.String("elapsed", begin.Format("2006-01-02 15:04:05")))
+	if len(i) > 0 {
+		for index, val := range i {
+			if index == 0 {
+				data = append(data, zap.Any(fmt.Sprintf("log_data"), val))
+			} else {
+				data = append(data, zap.Any(fmt.Sprintf("log_data_%d", index), val))
+			}
+		}
+	}
+	return
+}
 func Errorf(s string, i ...any) {
-	Default().Sugar().Errorf(s, i...)
+	data := logF(s, i...)
+	Default().Error("app_info", data...)
 }
 func Error(i ...any) {
-	Default().Sugar().Error(i...)
+	data := log(i...)
+	Default().Error("app_info", data...)
 }
 func Warnf(s string, i ...any) {
-	Default().Sugar().Warnf(s, i...)
+	data := logF(s, i...)
+	Default().Warn("app_info", data...)
 }
 func Warn(i ...any) {
-	Default().Sugar().Warn(i...)
+	data := log(i...)
+	Default().Warn("app_info", data...)
 }
 func Infof(s string, i ...any) {
-	Default().Sugar().Infof(s, i...)
+	data := logF(s, i...)
+	Default().Info("app_info", data...)
+
 }
 func Info(i ...any) {
-	Default().Sugar().Info(i...)
+	data := log(i...)
+	Default().Info("app_info", data...)
 }
 func Debugf(s string, i ...any) {
-	Default().Sugar().Debugf(s, i...)
+	data := logF(s, i...)
+	Default().Debug("app_info", data...)
 }
 func Debug(i ...any) {
-	Default().Sugar().Debug(i...)
+	data := log(i...)
+	Default().Debug("app_info", data...)
 }
