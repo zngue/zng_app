beeaee90582bf90c5cd78d0dfe739acc9afce373
zngue <zngfue@163.com>
2024-12-10 20:44:22 +0800

    update

--- log/log_sync.go
+++ log/log_sync.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/go-resty/resty/v2"
 )
@@ -8,8 +9,19 @@ import (
 type SaveLog struct {
 	Url string
 }
+type Data struct {
+	Level string `json:"level"`
+}
 
 func (s *SaveLog) Write(p []byte) (n int, err error) {
+	var rs Data
+	err = json.Unmarshal(p, &rs)
+	if err != nil {
+		return
+	}
+	if rs.Level != "error" {
+		return
+	}
 	client := resty.New()
 	req := client.R()
 	go func() {
