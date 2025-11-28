6a85e4d1f5971a4b82e02921ad565269c5a836ea
zngue <zngfue@163.com>
2025-11-28 13:51:20 +0800

    update

--- tag.sh
+++ tag.sh
@@ -9,7 +9,7 @@ git commit -m "update"
 git push
 sleep 3
 echo "推送完成 开始打标签"
-version=${1:-"v1.0.9"}
+version=${1:-"v1.0.10"}
 git tag -d "${version}"
 git push origin :refs/tags/"${version}"
 msg=${2:-"Release ${version} 新增日志项目"}
