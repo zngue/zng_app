f3b085054b338cba2847409a9e3488d440be158c
zngue <zngfue@163.com>
2025-12-02 15:10:53 +0800

    feat(bind): 添加日志接口的 Write 方法实现
    - 在 log 结构体中实现 Write 方法
    - 添加 panic("unimplemented") 作为占位符实现
    - 确保 Log 接口的所有方法都有对应实现

--- pkg/bind/log.go
+++ pkg/bind/log.go
@@ -7,6 +7,11 @@ type Log interface {
 type log struct {
 }
 
+// Write implements Log.
+func (l *log) Write(operation string, data any) {
+	panic("unimplemented")
+}
+
 var logImpl Log = &log{}
 
 func NewLog() Log {
